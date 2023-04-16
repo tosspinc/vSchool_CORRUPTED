57989478820167eb4535145c2035a607a9bb08c3
Arnold Jones <tosspinc@gmail.com>
2023-04-16 13:22:18 -0600

    correctly noted coding lines for my thorough  understanding

--- Assignments/level1/Node-Calculator/index.js
+++ Assignments/level1/Node-Calculator/index.js
@@ -11,28 +11,28 @@ function calculationChoice(){   // declares calculationChoice function
 }
 
 function mathCalculations(){            // declares mathCalculation function to actual calculations can be completed.
-    let result;                         // set result as a variable do results can be displayed and allows math to be completed.
+    let result;                         // set result as a variable so results can be displayed and allows math to be completed.
 
     if (typeOfCalculation === 'Add') {              //allows typeOfCalculation function to be validated correctly and run.
-        var firstNumber = readline.question("What is your 1st number? ");   //assigns input to firstNumber and stores input locally.
-                var secondNumber = readline.question("what is your 2nd number? ");  //assigns input to secondNumber and stores input locally.
-                result = Number(firstNumber) + Number(secondNumber);        // allows calculation to be completed and needs Number in front of object so addition can occur.
-                console.log("The Result is: " + result);                    // displays results to console.
+        var firstNumber = readline.question("What is your 1st number? ");   //assigns input from user to firstNumber and stores this input in local memory.
+        var secondNumber = readline.question("what is your 2nd number? ");  //assigns input from user to secondNumber and stores this input local memory.
+                result = Number(firstNumber) + Number(secondNumber);        // assigns data from firstNumber and secondNumber variables and does the math calculation, which requires Number before variables.
+                console.log("The Result is: " + result);                    // displays the string data and the data assigned to the result variable on the console.
         } else if (typeOfCalculation === 'Sub')  {                                                  //if sub is true then subtraction object will run.
           var firstNumber = readline.question("What is the 1st number you would like to use: ");    //assigns input to first number and stores input locally.
           var secondNumber = readline.question("what is the 2nd number you would like to use: ");   //assigns input to second number and stores input locally.
-                result = firstNumber - secondNumber;                                                //does the subtraction calculation.
-                console.log("The Result is: " + result);                                            //shows calculation result on console.
+                result = firstNumber - secondNumber;                                                //does the subtraction calculation and assigns data to result.
+                console.log("The Result is: " + result);                                            //displays the results assigned to the result variable in the console.
         } else if(typeOfCalculation === 'Mul') {                                                    // if Mul is true then multiplication object will run.
             var firstNumber = readline.question("What is the 1st number you would like to use: ");  //assigns input to first number and stores input locally.
             var secondNumber = readline.question("what is the 2nd number you would like to use: "); //assigns input to seconde number and stores input locally.
-                result = firstNumber * secondNumber;                                                //does the multiplication calculation
-                console.log("The Result is: " + result);                                            //shows the results on the terminal.
+                result = firstNumber * secondNumber;                                                //does the multiplication calculation and assigns data to result.
+                console.log("The Result is: " + result);                                            //displays the results from the result variable and displays the data on the terminal.
         } else if(typeOfCalculation === 'Div') {                                                    //if Div is true then division object will run.
             var firstNumber = readline.question("What is the 1st number you would like to use: ");  //assigns input to first number and stores input locally.
             var secondNumber = readline.question("what is the 2nd number you would like to use: "); //assigns input to second number and stores input locally.
-                result = firstNumber / secondNumber;                                                //does the division of the input.
-                console.log("The Result is: " + result);                                            //displays the results on the console.
+                result = firstNumber / secondNumber;                                                //does the division of the input and assigns data to result.
+                console.log("The Result is: " + result);                                            //displays the results from the result variable on the console.
     } else {
         console.log('Please choose: Add, Sub, Mul or Div only: ')                                   //this is error message if correct choice not made.
             calculationChoice()                                                                     //this calls the calculationsChoice function to start again so user can continue.
