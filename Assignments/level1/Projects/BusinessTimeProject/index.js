d9611e343f8ab5fea57851be2a91160880c08a51
Arnold Jones <tosspinc@gmail.com>
2023-05-03 08:48:02 -0600

    updating code and notation for Business time project of a company website. 05.04.2023.1043hrs.

--- Assignments/level1/Projects/BusinessTimeProject/index.js
+++ Assignments/level1/Projects/BusinessTimeProject/index.js
@@ -1,10 +1,33 @@
-$(document).ready(function() {
-    $('#search-category').change(function() {
-        let category = $(this).val();
-        let searchQuery = $('#search-query').val();
-        if (category === 'All'){
-            $('form').attr('action'),
+
+function showLocation(position){
+    let lattitude = position.coords.lattitude;
+    let longitude = position.coords.longitude;
+    let url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2lat=" + lattitude + "&lon=" + longitude;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let city = data.address.city || data.address.town || data.address.village;
+            let state = data.address.state || data.address.province;
+            let country = data.address.country;
+            document.getElementById("location.text").innerHTML = city + ", " + state + ",  " + country;
+        })
+
+        .catch(error => {
+            console.log(error);
+            document.getElementById("location.text").innerHTML = "Unable to detect your location";
+        });
+    }
+
+    function showError(error){
+        console.log(error);
+        document.getElementById("location.text").innerHTML = "Unable to detect your location";
+    }
+
+    if (navigation.geolocation){
+        navigator.geolocation.getCurrentPosition(showLocation, showError);
+        else {
+            document.getElementById("location.text").innerHTML = "Geolocation is not supported by this browser";
         }
-    });
-};
+    }
 
