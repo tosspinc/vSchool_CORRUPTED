aa460c0f0a488758d94bf417f8d4daa978d0cfb2
Arnold Jones <tosspinc@gmail.com>
2023-05-18 20:11:46 -0600

    updated coding for Mario Pest Control assigment with notation for my future reference. 05.23.2023 at 1120 hours.

--- Assignments/level2/Assignment/MarioPestControl/MPControl.js
+++ Assignments/level2/Assignment/MarioPestControl/MPControl.js
@@ -1,72 +1,67 @@
-const tBaddiesCaught = document.getElementById('tBaddiesCaught');
-const totalInvoiceSubmit = document.getElementById('button'); 
-const clearInvoiceSubmit = document.getElementById('clearInvoice'); 
-const totalInvoiceAmount = document.getElementById('totalInvoiceAmount'); 
+const totalInvoiceSubmit = document.getElementById('button');               //declares totalInvoiceSubmit as a constant variable and assigns button to it.
+const clearInvoiceSubmit = document.getElementById('clearInvoice');         //declares clearInvoiceSubmit as a constant variable and assigns clearInvoice to it.
+const totalInvoiceAmount = document.getElementById('totalInvoiceAmount');   //declares totalInvoiceAmount as a constant variable and assigns totalInvoiceAmount to it.
 
-const totalBaddies = document.getElementById('tBaddiesCaught');
+const tOwed = document.getElementById('tOwed');                             //declares tOwed as a constant variable and assigns tOwed to it.
 
-const goombaQty = document.getElementById('goombaQtyTotalCaught'); 
-const boboombsQty = document.getElementById('boboombsQtyTotalCaught'); 
-const cheepCheepsQty = document.getElementById('cheepCheepsQtyTotalCaught'); 
+const goombaQty = document.getElementById('goombaQtyTotalCaught');          //declares goombaQty as a constant variable and goombaQtyTotalCaught to it.
+const goombaTotalPriceOwed = document.getElementById('goombaTotalPriceOwed'); //declares goombaTotalPriceOwed as a constant variable and assigns goombaTotalPriceOwed to it.
+const goombaQtyPrice = document.getElementById('gQtyCost');                 //declares goombaQtyPrice as a constant variable and assigns gQtyCost to it.
 
-const goombaTotalPriceOwed = document.getElementById('goombaTotalPriceOwed'); 
-const boboombsTotalPriceOwed = document.getElementById('boboombsTotalPriceOwed'); 
-const cheepCheepsTotalPriceOwed = document.getElementById('cheepCheepsTotalPriceOwed'); 
+const boboombsQty = document.getElementById('boboombsQtyTotalCaught');      //declares boboombsQty as a constant variable and boboombsQtyTotalCaught to it.
+const boboombsTotalPriceOwed = document.getElementById('boboombsTotalPriceOwed'); //declares boboombsTotalPriceOwed as a constant variable and assigns boboombsTotalPriceOwed to it.
+const boboombsQtyPrice = document.getElementById('bQtyCost');               //declares boboombsQtyPrice as a constant variable and assigns bQtyCost to it.
 
-const goombaQtyPrice = document.getElementById('gQtyCost'); 
-const boboombsQtyPrice = document.getElementById('bQtyCost'); 
-const cheepCheepsQtyPrice = document.getElementById('cQtyCost'); 
+const cheepCheepsQty = document.getElementById('cheepCheepsQtyTotalCaught'); //declares cheepCheepsQty as a constant variable and cheepCheepsQtyTotalCaught to it.
+const cheepCheepsTotalPriceOwed = document.getElementById('cheepCheepsTotalPriceOwed'); //declares cheepCheepsTotalPriceOwed as a constant variable and assigns cheepCheepsTotalPriceOwed to it.
+const cheepCheepsQtyPrice = document.getElementById('cQtyCost');            //declares cheepCheepsQtyPrice as a constant variable and assigns cQtyCost to it.
 
-function calculateTotalPrice() { 
-    const goombaQty = parseInt(goombaQtyTotalCaught.value); 
-    const boboombsQty = parseInt(boboombsQtyTotalCaught.value); 
-    const cheepCheepsQty = parseInt(cheepCheepsQtyTotalCaught.value); 
 
-    const goombaPrice = parseInt(gQtyCost.value); 
-    const boboombsPrice = parseInt(bQtyCost.value); 
-    const cheepCheepsPrice = parseInt(cQtyCost.value); 
-
-    goombaTotalPriceOwed = goombaQty * goombaPrice; 
-    boboombsTotalPriceOwed = boboombsQty * boboombsPrice; 
-    cheepCheepsTotalPriceOwed = cheepCheepsQty * cheepCheepsPrice; 
-
-    tBaddiesCaught = goombaQty + boboombsQty + cheepCheepsQty;
+function calculateTotalPrice() {                                            //declares calculateTotalPrice() as a function.
+    const goombaQty = parseInt(goombaQtyTotalCaught.value);                 //declares goombaQty as a constant variable and passes value of goombaQtyCaught to it.
+    const goombaPrice = parseInt(gQtyCost.value);                           //declares goombaPrice as a constant variable and passes value of gQtyCost to it.
+    goombaTotalPriceOwed = goombaQty * goombaPrice;                         //mulitplie goombaQty and goombaPrice and assigns value to goombaTotalPriceOwed.
+    const boboombsQty = parseInt(boboombsQtyTotalCaught.value);             //declares boboombsQty as a constant variable and passes value of boboombsQtyTotalCaught to it.
+    const boboombsPrice = parseInt(bQtyCost.value);                         //declares boboomsPrice as a constant variable and passes value of bQtyCost to it.
+    boboombsTotalPriceOwed = boboombsQty * boboombsPrice;                   //mulitiplies boboombsQty and boboombsPrice and assigns the value to boboombsTotalPriceOwed.
+    const cheepCheepsQty = parseInt(cheepCheepsQtyTotalCaught.value);       //declares cheepCheepsQty as a constant variable and passes value of cheepCheepsQtyTotalCaught to it. 
+    const cheepCheepsPrice = parseInt(cQtyCost.value);                      //declares cheepCheepsPrice as a constant variable and passes value of cQtyCost to it.
+    cheepCheepsTotalPriceOwed = cheepCheepsQty * cheepCheepsPrice;          //mulitiplies cheepCheepsQty and cheepCheepsPrice and assigns its value to cheepCheepsTotalPriceOwed.
+    tOwed.value = parseInt(goombaTotalPriceOwed.value) + parseInt(boboombsTotalPriceOwed.value) + parseInt(cheepCheepsTotalPriceOwed.value); //adds the value of goombaTotalPriceOwed, boboombsTotalPriceOwed and cheepCheepsTotalPriceOwed and assigns its value to tOwed.
 } 
 
-function clearInvoice() { 
-    goombaQty.value = 0; 
-    goombaTotalPriceOwed.value = 0; 
-    boboombsQty.value = 0; 
-    boboombsTotalPriceOwed.value = 0; 
-    cheepCheepsQty.value = 0; 
-    cheepCheepsTotalPriceOwed.value = 0; 
-    totalBaddies.value = 0; 
-    totalInvoiceAmount.value = 0; 
+function clearInvoice() {                   //declares clearInvoice() object as a function.
+    goombaQty.value = 0;                    //sets the initial value of goombaQty to zero.
+    goombaTotalPriceOwed.value = 0;         //sets the initial value of goombaTotalPriceOwed to zero.
+    boboombsQty.value = 0;                  //sets the initial value of boboombsQty to zero.
+    boboombsTotalPriceOwed.value = 0;       //sets the initial value of boboombsTotalPriceOwed to zero.
+    cheepCheepsQty.value = 0;               //sets the initial value of cheepCheepsQty to zero.
+    cheepCheepsTotalPriceOwed.value = 0;    //sets the initial value of cheepCheepsTotalPriceOwed to zero.
+    totalInvoiceAmount.value = 0;           //sets the initial value of totalInvoiceAmount to zero.
+    tOwed.value = 0;                        //set the initial value of tOwed to zero.
 };
 
-totalInvoiceAmount.value = goombaTotalPriceOwed.value + boboombsTotalPriceOwed.value + cheepCheepsTotalPriceOwed.value;
-goombaQty.addEventListener('change', (e) => {
-    goombaTotalPriceOwed.value = goombaQty.value * goombaQtyPrice.value;
+goombaQty.addEventListener('change', (e) => {                           //listens for the date to change in the goombaQty input box and declares an empty function.
+    goombaTotalPriceOwed.value = goombaQty.value * goombaQtyPrice.value;    //mulitplies the values of goombaQty and goombaQtyPrice and assigns the value to goombaTotalPriceOwed.
+    tOwed.value = parseInt(goombaTotalPriceOwed.value) + parseInt(boboombsTotalPriceOwed.value) + parseInt(cheepCheepsTotalPriceOwed.value); //adds the value from goombaTotalPriceOwed, boboombsTotalPriceOwed and cheepCheepsTotalPriceOwed and assigns it to tOwed.
 }); 
 
 boboombsQty.addEventListener('change', (e) => {
     boboombsTotalPriceOwed.value = boboombsQty.value * boboombsQtyPrice.value;
+    tOwed.value = parseInt(goombaTotalPriceOwed.value) + parseInt(boboombsTotalPriceOwed.value) + parseInt(cheepCheepsTotalPriceOwed.value);
 });
 
 cheepCheepsQty.addEventListener('change', (e) => {
     cheepCheepsTotalPriceOwed.value = cheepCheepsQty.value * cheepCheepsQtyPrice.value;
+    tOwed.value = parseInt(goombaTotalPriceOwed.value) + parseInt(boboombsTotalPriceOwed.value) + parseInt(cheepCheepsTotalPriceOwed.value);
 });
 
-totalBaddies.addEventListener('change', (e) => {
-    tBaddiesCaught.value = cheepCheepsQty.value + boboombsQty.value + goombaQty.value;
-});
-
-totalInvoiceSubmit.addEventListener('click', (e) => { 
-    e.preventDefault(); 
-    if (totalInvoiceAmount.value > "0") { 
-        clearInvoice(); 
+totalInvoiceSubmit.addEventListener('click', (e) => { //declares and empty function and listens for the submit button to be entered then assigns that to totalInvoiceSubmit.
+    e.preventDefault(); //prevents from from clearing.
+    if (tOwed.value > "0") {    //validates that tOwed has a value larger than zero and if it does then it display the following alert message.
+        clearInvoice();   //calls the clearInvoice function.
         alert("You have submitted your invoice to Princess Peach. Please wait 30 days for payment."); 
-    } else { 
+    } else {    //if totalInvoiceSubmit does not have any data then the following alert message is displayed.
         alert("You have not entered any Baddies Found!"); 
     }  
 });
