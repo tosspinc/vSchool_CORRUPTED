88e40a3586ca9e840c4ba443c390f2f0c3ed34b1
Arnold Jones <tosspinc@gmail.com>
2023-06-11 17:24:58 -0600

    completed notes for assignments up through JSON story point 153.5 on 06.12.2023 at 1910 hours.

---
+++ Assignments/level2/Lessons/http/app.js
@@ -0,0 +1,16 @@
+//HTTP Lesson  Http: hyptertext Transfer Protocol.
+//request response cycle.
+//enter an address: https://www.amazon.com
+//servers:
+//Main Purpose - listens for a request and sends a response.
+//server types - local and remote.
+//live server is a type of a local server.
+//API : Application Program Interface.
+//an API defines how software components interact. They receive
+//information from the client. processes that task
+//request and then sends back a response to the requestor.
+//client in a restaurant would be the customer ordering food. 
+//server in a restaurant is the server who takes the order and sends to the kitchen.
+//api : kitchen in restaurant that processes order : prepares and cooks food.
+//server: server delivers food back to customer.
+//client: customer recieves order from server.
