3076a732ae9ca0eed545e7324260b5c726905f5e
Arnold Jones <tosspinc@gmail.com>
2023-07-01 19:16:05 -0600

    completed lesson for get request with axios. 07.01.2023 at 1916 hours.

---
+++ Assignments/level2/Lessons/getRequestWithAxios/index.js
@@ -0,0 +1,21 @@
+// get requests with axios.
+//notes for help understanding - https://coursework.vschool.io/v-schools-todo-api-documentation/
+
+// url: https://api.vschool.io/arnoldjones/todo
+
+//get all - an array of items.  gets json data.
+
+axios.get("https://api.vschool.io/arnoldjones/todo")
+    .then(response => {
+        for(let i = 0; i < response.data.length; i++){
+            const h1 = document.createElement('h1')
+            h1.textContent = response.data[i].title
+            document.body.appendChild(h1)
+        }
+    })
+    .catch(error => console.log(error))
+
+//get one - a single item.
+//axios.get("https://api.vschool.io/arnoldjones/todo/648a681478fe452c11499701")
+//    .then(response => console.log(response.data))
+//    .catch(error => console.log(error))
