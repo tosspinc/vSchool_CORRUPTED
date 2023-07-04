440784da0a296aa36bb9844dafc056dda62ee0cc
Arnold Jones <tosspinc@gmail.com>
2023-07-04 09:54:05 -0600

    completed coding and notation for level 2 axio capstone project. TA Anjanique Mackey vSchool TA assisted in correcting code on 07.07.2023 at 2043 hours.

--- Assignments/level2/Project/levelTwoCapstone/index.js
+++ Assignments/level2/Project/levelTwoCapstone/index.js
@@ -1,7 +1,7 @@
 function getData() {
   axios.get("https://api.vschool.io/arnoldjones/todo")              /*calls data from my vschool profile*/
   .then(response => {                                               /*declares function*/
-    for(let i = 0; i < response.data.length; i++){                 /*gets all data from vschool database and starts for loop*/
+    for(let i = 0; i < response.data.length; i++){                  /*gets all data from vschool database and starts for loop*/
       const title = document.createElement('h2');                   /*declares title as variable and assigns to h2*/
       title.textContent = response.data[i].title;                   /*sets the content retrieved and assigns it to the title object.*/
       document.body.appendChild(title);                             /*assigns data to html document in the body section.*/
@@ -24,27 +24,23 @@ function getData() {
                 
       const completedValue = document.createElement('h4');          /*declares completedValue as variable and assigns it to h4*/
       completedValue.textContent = response.data[i].completedValue; /*sets the content retrieved and assigns it to the completedValue object.*/
-      document.body.appendChild(completedValue);                    /*assigns data to html document in the body section.*/
-    };           
-    
-    const completedActionTrue = {                                   /*declares completedActionTrue as a variable.*/ 
-      completed: true                                               /*verifies that object is true.*/
-    };
+      document.body.appendChild(completedValue);
 
-    const completeButton = document.createElement('input');         /*declares completeButtons as a variable and assigns input data to it.*/
-    completeButton.setAttribute("id", `completeButton${response.data[i]._id}`);
-    completeButton.type = "checkbox";                               /*sets completeButtons as a checkbox.*/
+      const completeButton = document.createElement('input');         /*declares completeButtons as a variable and assigns input data to it.*/
+      completeButton.setAttribute("id", `completeButton${response.data[i]._id}`);
+      completeButton.type = "checkbox";/*assigns data to html document in the body section.*/
+
+                                   /*sets completeButtons as a checkbox.*/
 
     const completeRequest = function(id) {                          /*declares completeRequest as a variable and id as a function.*/
       if (`completeButton${response.data[i]._id}.checked`) {        /*if statement verifies that id is marked as checked.*/
          axios.put(`https://api.vschool.io/arnoldjones/todo/${id}`, completedActionTrue) /*adds data entry to my vschool database.*/
             .then(response => {                                   
-                getData(),                                        /*calls the getData function which pulls data from my vschool database file.*/
-                window.location.reload();                         /*refreshes the browser with the new data.*/
+                getData();                                          /*calls the getData function which pulls data from my vschool database file.*/
             })
-            .catch(error => console.log(error.data));             /*if there is an error then it runs the following line of code.*/
+            .catch(error => console.log(error.data));               /*if there is an error then it runs the following line of code.*/
       } else {
-        console.log("Checkbox is not checked..");                 /*if there is an error displays this message.*/
+        console.log("Checkbox is not checked..");                   /*if there is an error displays this message.*/
       }
     };
     completeButton.addEventListener("change", () => completeRequest(response.data[i]._id));
@@ -56,41 +52,40 @@ function getData() {
 
     //DELETE REQUEST
     const deleteRequest = function(id) {                            /*declares deleteReques as a variable and assigns id function data to it.*/
-      axios.delete(`https://api.vschool.io/arnoldjones/todo/${id}`)
+      axios.delete(`https://api.vschool.io/arnoldjones/todo/${_id}`)
         .then(response => {
-            getData(),                                              /*calls the getData function.*/
-            window.location.reload();                               /*refreshes the webpage with the data pulled from my vschool database file.*/
+            getData();                                              /*calls the getData function.*/
         })
         .catch(error => console.log(error.data));                   /*if an error occurs, the error message is displayed.*/
     };
     deleteButton.addEventListener("click", ()=> deleteRequest(response.data[i]._id));
     document.body.appendChild(deleteButton);
-  })
+  }})
   .catch(error => console.log(error));
 };
-
 getData();                                                          /*calls the getData function.*/
 
 const toDoform = document.toDoform;                                 /*declares toDoform as a variable and assigns data to html document.*/
 
 toDoform.addEventListener("submit", function(e){
-    e.preventDefault()                                              /*stops webpage from refreshing.*/
-    const newTodo = {                                               /*declares newTodo as a variable.*/
-        title: toDoform.title.value,                                /*assigns the value in the title text box to title object.*/
-        price: toDoform.price.value,                                /*assigns the value in the price text box to price object.*/
-        description: toDoform.description.value,                    /*assigns the value in the  description text box to description object.*/
-        imgUrl: toDoform.imgUrl.value,                              /*assigns the value in the  image text box to imgUrl object.*/
-        completed: false                                            /*initial newTodo completion is set to false status.*/
-    };
-    toDoform.title.value = "",                                      /*sets title data value to empty.*/
-    toDoform.price.value = "",                                      /*sets price data value to empty.*/
-    toDoform.description.value = "",                                /*sets description data value to empty.*/
-    toDoform.imgUrl.value = ""                                      /*sets image url data value to empty.*/
+  e.preventDefault()                                              /*stops webpage from refreshing.*/
+  const newTodo = {                                               /*declares newTodo as a variable.*/
+      title: toDoform.title.value,                                /*assigns the value in the title text box to title object.*/
+      price: toDoform.price.value,                                /*assigns the value in the price text box to price object.*/
+      description: toDoform.description.value,                    /*assigns the value in the  description text box to description object.*/
+      imgUrl: toDoform.imgUrl.value,                              /*assigns the value in the  image text box to imgUrl object.*/
+      completed: false                                            /*initial newTodo completion is set to false status.*/
+  };
+  toDoform.title.value = "",                                      /*sets title data value to empty.*/
+  toDoform.price.value = "",                                      /*sets price data value to empty.*/
+  toDoform.description.value = "",                                /*sets description data value to empty.*/
+  toDoform.imgUrl.value = ""                                      /*sets image url data value to empty.*/
 
-    axios.post("https://api.vschool.io/arnoldjones/todo", newTodo)  /*posts data to my vschool database file and assigns it to newTodo.*/
-        .then(response => {
-            getData(),                                              /*calls the getData function*/
-            window.location.reload()                                /*loads the data to the browser.*/
-        })
-        .catch(error => console.log(error.data.title))              /*catches errors and displays the error.*/
+  axios.post("https://api.vschool.io/arnoldjones/todo", newTodo)  /*posts data to my vschool database file and assigns it to newTodo.*/
+    .then(response => {
+        getData(),                                              /*calls the getData function*/
+        window.location.reload()                                /*loads the data to the browser.*/
+    })
+    .catch(error => console.log(error.data.title))              /*catches errors and displays the error.*/
 });
+  
