82a9e4a3a22fc339bd45c9ceb9de320774360808
Arnold Jones <tosspinc@gmail.com>
2023-08-18 01:14:10 -0600

    added vanilla code per react lesson hour 182.5 in level 3 and so it will run.  completed 08.18.2023 at 1047 hours.

--- Assignments/Level3/Lessons/React Introduction/index.js
+++ Assignments/Level3/Lessons/React Introduction/index.js
@@ -1,52 +1,58 @@
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+//ReactDom.render(<h1 className="header">hello, React!</h1>, document.getElementById("root"))
+// function Navbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <a className="navbar-brand" href="#">Navbar</a>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span className="navbar-toggler-icon"></span>
+//             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
-}
+//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <ul className="navbar-nav mr-auto">
+//                 <li className="nav-item active">
+//                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li className="nav-item dropdown">
+//                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                     Dropdown
+//                     </a>
+//                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+//                     <a className="dropdown-item" href="#">Action</a>
+//                     <a className="dropdown-item" href="#">Another action</a>
+//                     <div className="dropdown-divider"></div>
+//                     <a className="dropdown-item" href="#">Something else here</a>
+//                     </div>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link disabled" href="#">Disabled</a>
+//                 </li>
+//                 </ul>
+//                 <form className="form-inline my-2 my-lg-0">
+//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//                 </form>
+//             </div>
+//         </nav>
+//     )
+// }
 
-function MainContent() {        //called MainContent as a function.
-    return(                     //this allows the contents to be displayed in the dom.
-    <h1>How is this?</h1>       //h1 
-    )
-}
+// function MainContent() {        //called MainContent as a function.
+//     return(                     //this allows the contents to be displayed in the dom.
+//     <h1>How is this?</h1>       //h1 
+//     )
+// }
 
-ReactDOM.render(
-    <div>
-        <Navbar />
-        <MainContent />         
-    </div>,
-    document.getElementById("root"))
+// ReactDOM.render(
+//     <div>
+//         <Navbar />
+//         <MainContent />         
+//     </div>,
+// //    document.getElementById("root"))
+
+ const h1 = document.createElement("h1");
+h1.textContent = "this is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
