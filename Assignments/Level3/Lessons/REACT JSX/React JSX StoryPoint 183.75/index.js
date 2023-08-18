2abd1ab793bcb5f40a6ae1d05bcb4a1158bf88fb
Arnold Jones <tosspinc@gmail.com>
2023-08-18 15:13:10 -0600

    added notation so I know how to use this in the future. completed on 08.20.2023 at 1145 hours.

--- Assignments/Level3/Lessons/REACT JSX/React JSX StoryPoint 183.75/index.js
+++ Assignments/Level3/Lessons/REACT JSX/React JSX StoryPoint 183.75/index.js
@@ -1,5 +1,8 @@
 //with the two following lines of code active it does not display in the browser.
 //when I comment out the next two lines of code the program renders in the browser (dom document).
+
+//***The reason is that when the following two lines of code are notated out it is allowing jsx to render it through ReactDOM.render and is not using React at all.
+
 //import React from "react"
 //import ReactDOM from "react-dom"
 
