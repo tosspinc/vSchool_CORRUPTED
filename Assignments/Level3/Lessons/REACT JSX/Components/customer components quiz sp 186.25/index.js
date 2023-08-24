39c1c4df8ff126164a662ddc430427b746a0b8db
Arnold Jones <tosspinc@gmail.com>
2023-08-24 11:20:27 -0600

    added code so program works and some notation. completed 08.24.2023 at 1121 hours.

--- Assignments/Level3/Lessons/REACT JSX/Components/customer components quiz sp 186.25/index.js
+++ Assignments/Level3/Lessons/REACT JSX/Components/customer components quiz sp 186.25/index.js
@@ -1,23 +1,23 @@
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
+// //Part 2: 
+// - Add a `header` element with a nested `nav` element. Inside the `nav`,
+//   include a `img` element with the image of the React logo inside
+//   (src="./react-logo.png") and make sure to set the width to something
+//   more manageable so it doesn't take up the whole screen
+// - Add an `h1` with some text describing the page. (E.g. "Reasons
+//   I'm excited to learn React"). Place it above the ordered list.
+// - Add a `footer` after the list that says: 
+//     "© 20xx <last name here> development. All rights reserved."
+//  */
 
-import React from "react"
-import ReactDOM from "react-dom"
+//import React from "react"
+//import ReactDOM from "react-dom"
 
 function Page() {
     return (
         <div>
             <header>
                 <nav>
-                    <img src="./react-logo.png" width="40px" />
+                    <img src="./IMGS/react.png" width="40px" />
                 </nav>
             </header>
             <h1>Reasons I'm excited to learn React</h1>
