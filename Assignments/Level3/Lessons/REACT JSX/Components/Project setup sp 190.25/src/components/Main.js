115e616e456ab7d20891e99da21b83c4ba122d0c
Arnold Jones <tosspinc@gmail.com>
2023-09-07 20:02:28 -0600

    completed coding for React Project setup - sp 190.25 on 09.11.2023 @ 1137 hours.

---
+++ Assignments/Level3/Lessons/REACT JSX/Components/Project setup sp 190.25/src/components/Main.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Main() {
+    <h1>Main content info goes here.</h1>
+}
---
+++ Assignments/Level3/Lessons/REACT JSX/Components/Project setup sp 190.25/src/components/Navbar.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Navbar() {
+    <h1>Navbar info goes here.</h1>
+}
