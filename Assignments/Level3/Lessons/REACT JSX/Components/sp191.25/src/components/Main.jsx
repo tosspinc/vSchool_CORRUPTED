e1f824dac1a2f43987735ea63590d5919c27fb55
Arnold Jones <tosspinc@gmail.com>
2023-09-18 16:08:08 -0600

    changed coding and notated changes made in index.css file on 09.18.2023 at 1608 hours.

--- Assignments/Level3/Lessons/REACT JSX/Components/sp191.25/src/components/Main.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Components/sp191.25/src/components/Main.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Main() {
     return (
         <main>
-            <h1>Facts about Tosspi.</h1>
+            <h1 className="main--title">Facts about Tosspi.</h1>
 
-            <ul>
+            <ul className="main--facts">
                 <li>Parent company is: Tossp Inc.</li>
                 <li>Founded in 2019.</li>
                 <li>We sell New and Previously Owned Merchandise.</li>
