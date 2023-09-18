a914b0304fbfb3aa62e96e0206ea272581dba938
Arnold Jones <tosspinc@gmail.com>
2023-09-18 14:28:36 -0600

    completed 191.25 with help from TA Kyle shutt. 09.18.2023 at 1429 hours.

--- Assignments/Level3/Lessons/REACT JSX/Components/sp191.25/src/App.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Components/sp191.25/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import NewTosspiLogo from '../public/images/NewTosspiLogo.jpg';
 import Navbar from './components/Navbar';
-//import Main from './components/Main';
+import Main from './components/Main';
 //import Footer from './components/Footer';
 
 function App() {
@@ -13,31 +13,17 @@ function App() {
   return (
     <>
       <Navbar />
-      {/*<Main />*/}
-      <div>        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Main />
       
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     {/*<Footer />*/}
     </>
   )
 }
 
 export default App
-
---
+++ Assignments/Level3/Lessons/REACT JSX/Components/sp191.25/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Facts about Tosspi.</h1>
+
+            <ul>
+                <li>Parent company is: Tossp Inc.</li>
+                <li>Founded in 2019.</li>
+                <li>We sell New and Previously Owned Merchandise.</li>
+            </ul>
+        </main>
+    )
+}
