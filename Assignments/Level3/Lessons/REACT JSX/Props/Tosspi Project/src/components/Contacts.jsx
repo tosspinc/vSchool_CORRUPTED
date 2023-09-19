abd23474dbb8cb532dced2c9470f403dfb198956
Arnold Jones <tosspinc@gmail.com>
2023-09-19 06:55:30 -0600

    updated code and notation. completed 09.23.2023 at 1136 hours.

---
+++ Assignments/Level3/Lessons/REACT JSX/Props/Tosspi Project/src/components/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <div className='contact-card'>
+            <img src='../images/TosspiFoundation.jpg'/>
+            <h2>The Tosspi Foundation</h2>
+            <div className='contact-phone'>
+                <img src='../images/PhoneIcon.png' />
+                <p>801-347-5153</p>
+            </div>
+        </div>
+    )
+}
---
+++ Assignments/Level3/Lessons/REACT JSX/Props/Tosspi Project/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Facts about Tosspi - The One Stop Shopping Place Inc.</h1>
+
+            <ul className="main--facts">
+                <li>Parent company is: Tossp Inc.</li>
+                <li>Founded in 2019.</li>
+                <li>We sell New and Previously Owned Merchandise.</li>
+            </ul>
+        </main>
+    )
+}
