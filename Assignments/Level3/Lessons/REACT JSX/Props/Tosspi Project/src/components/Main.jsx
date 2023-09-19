b01bb8bbd8810c1a3538e486db7dcb15180a3568
Arnold Jones <tosspinc@gmail.com>
2023-09-19 14:57:07 -0600

    changed color in text and navbar and updated text. completed 09.25.2023 at 0946hours.

--- Assignments/Level3/Lessons/REACT JSX/Props/Tosspi Project/src/components/Main.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Props/Tosspi Project/src/components/Main.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Main() {
     return (
         <main>
-            <h1 className="main--title">Facts about Tosspi - The One Stop Shopping Place Inc.</h1>
+            <h1 className="main--title">Facts about Tosspi - The One Stop Shopping Place.</h1>
 
             <ul className="main--facts">
-                <li>Parent company is: Tossp Inc.</li>
+                <li>Parent company is: Tossp Inc. DBA Tosspi</li>
                 <li>Founded in 2019.</li>
                 <li>We sell New and Previously Owned Merchandise.</li>
             </ul>
