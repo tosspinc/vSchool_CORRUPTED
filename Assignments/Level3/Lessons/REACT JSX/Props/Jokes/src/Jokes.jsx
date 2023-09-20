75a7829d18de22bae37fd989c47f7adf7c535b47
Arnold Jones <tosspinc@gmail.com>
2023-09-20 05:48:16 -0600

    updated code and notation in Jokes.jsx file appropriately. completed 09.25.2023 at 2339 hours.

--- Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
@@ -5,7 +5,7 @@ export default function Jokes(props) {
         <>
             <div className="Dad_Jokes">
                 {props.setup && <h3>Setup: {props.setup}</h3>} {/*if props.setup is a truthy value then render what is on the right side of the and operator (&&) add the h3 line of code. If it is falsy (there is no setup code then it won't render anything.*/}
-                {props.punchline && <p>Punchline: {props.punchline}</p>} {/*if props.punchline is a truthy value then render what is on the right side of the and operator (&&) add the p line of code. If it is falsy (there is no setup code then it won't render anything.*/}
+                {props.punchline && <p>Punchline: {props.punchline}</p>} {/*if props.punchline is a truthy value then render what is on the right side of the and operator (&&) add the <p> - paragraph line of code. If it is falsy (there is no punchline code then it won't render the punchline paragraph line of code.*/}
                 <hr />
             </div>
         </>
