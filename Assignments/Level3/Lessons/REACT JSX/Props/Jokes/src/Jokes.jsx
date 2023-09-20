9b2a0bd8a22941d94bceffad915116534e894d8c
Arnold Jones <tosspinc@gmail.com>
2023-09-20 05:42:41 -0600

    added code and notation to Jokes.jsx file regarding if there is a setup line of oce then it will render and it there is not then it will only render the punchline line of code.

--- Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
@@ -5,7 +5,7 @@ export default function Jokes(props) {
         <>
             <div className="Dad_Jokes">
                 {props.setup && <h3>Setup: {props.setup}</h3>} {/*if props.setup is a truthy value then render what is on the right side of the and operator (&&) add the h3 line of code. If it is falsy (there is no setup code then it won't render anything.*/}
-                <p>punchline: {props.punchline}</p>
+                <p>Punchline: {props.punchline}</p>
                 <hr />
             </div>
         </>
