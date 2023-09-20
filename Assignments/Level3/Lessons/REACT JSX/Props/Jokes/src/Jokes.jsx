829acf3d185a90d4d99f6bb1d5fce567ec8bc8e6
Arnold Jones <tosspinc@gmail.com>
2023-09-20 05:46:18 -0600

    updated code and notation in Jokes.jsx regarding adding else statement in jsx and whether or not to run it.

--- Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
+++ Assignments/Level3/Lessons/REACT JSX/Props/Jokes/src/Jokes.jsx
@@ -5,7 +5,7 @@ export default function Jokes(props) {
         <>
             <div className="Dad_Jokes">
                 {props.setup && <h3>Setup: {props.setup}</h3>} {/*if props.setup is a truthy value then render what is on the right side of the and operator (&&) add the h3 line of code. If it is falsy (there is no setup code then it won't render anything.*/}
-                <p>Punchline: {props.punchline}</p>
+                {props.punchline && <p>Punchline: {props.punchline}</p>} {/*if props.punchline is a truthy value then render what is on the right side of the and operator (&&) add the p line of code. If it is falsy (there is no setup code then it won't render anything.*/}
                 <hr />
             </div>
         </>
